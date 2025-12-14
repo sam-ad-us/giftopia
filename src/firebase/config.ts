2bca546d8341827bc49cc66a4aa35347b41dedc1
Mr Lyrical <mrlyrical777@gmail.com>
2025-12-14 15:09:43 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7539630518-ae89f",
+  "appId": "1:784649478152:web:7e9c6250d566dfb28031bd",
+  "apiKey": "AIzaSyAvzXA88IG-5TlqvwZ3frFTOvQijPaBqzE",
+  "authDomain": "studio-7539630518-ae89f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "784649478152"
+};
