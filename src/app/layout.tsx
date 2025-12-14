811cec1f8e6bc24d85aba50ad2b3e0f7bf62801b
Mr Lyrical <mrlyrical777@gmail.com>
2025-12-14 16:23:29 +0000

    in the admin pannel not show header and footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
@@ -6,32 +8,48 @@ import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import { FirebaseClientProvider } from '@/firebase';
 import SubHeader from '@/components/SubHeader';
+import { usePathname } from 'next/navigation';
 
-export const metadata: Metadata = {
-  title: 'Giftopia',
-  description: 'The perfect gift for every occasion.',
-};
+// export const metadata: Metadata = {
+//   title: 'Giftopia',
+//   description: 'The perfect gift for every occasion.',
+// };
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const pathname = usePathname();
+  const isAdminPage = pathname.startsWith('/admin-secret-sam01');
+
   return (
     <html lang="en" className="scroll-smooth">
       <head>
+        <title>Giftopia</title>
+        <meta
+          name="description"
+          content="The perfect gift for every occasion."
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&family=PT+Sans:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
       </head>
       <body className="font-body antialiased">
         <FirebaseClientProvider>
           <CartProvider>
-            <div className="flex flex-col min-h-screen">
-              <Header />
-              <SubHeader />
+            <div className="flex min-h-screen flex-col">
+              {!isAdminPage && <Header />}
+              {!isAdminPage && <SubHeader />}
               <main className="flex-grow">{children}</main>
-              <Footer />
+              {!isAdminPage && <Footer />}
             </div>
             <Toaster />
           </CartProvider>
