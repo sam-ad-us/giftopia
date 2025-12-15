c099d9cbddcb368cc623d03cc3d216a04c68043c
Mr Lyrical <mrlyrical777@gmail.com>
2025-12-15 13:50:57 +0000

    ## Error Type Recoverable Error
    ## Error Message
    Hydration failed becau

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,16 +31,6 @@ export default function RootLayout({
           name="description"
           content="The perfect gift for every occasion."
         />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Playfair+Display:wght@700&display=swap"
-          rel="stylesheet"
-        />
       </head>
       <body className="font-body antialiased">
         <FirebaseClientProvider>
