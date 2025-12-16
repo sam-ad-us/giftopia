72702dc2f2e4093354f867c3b5be2872674925f8
Mr Lyrical <mrlyrical777@gmail.com>
2025-12-16 07:04:01 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/next.config.ts
+++ src/next.config.ts
@@ -24,8 +24,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https'
-        ,
+        protocol: 'https',
         hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
@@ -41,7 +40,7 @@ const nextConfig: NextConfig = {
         hostname: 'ik.imagekit.io',
         port: '',
         pathname: '/**',
-      }
+      },
     ],
   },
 };
