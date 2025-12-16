c38cc90670597bc699f9a96b90d129c73a57bd54
Mr Lyrical <mrlyrical777@gmail.com>
2025-12-16 07:43:17 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://ik.imag

--- src/next.config.ts
+++ src/next.config.ts
@@ -35,6 +35,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'ik.imagekit.io',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
