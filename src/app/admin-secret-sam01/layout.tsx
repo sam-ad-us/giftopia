db4e4e262ada043d6416a395099cdf756bf1809c
Mr Lyrical <mrlyrical777@gmail.com>
2025-12-17 12:23:57 +0000

    in admin page - make dashboard more scrolable

--- src/app/admin-secret-sam01/layout.tsx
+++ src/app/admin-secret-sam01/layout.tsx
@@ -184,7 +184,7 @@ function ProtectedAdminLayout({ children }: { children: ReactNode }) {
               </DropdownMenu>
             </div>
           </header>
-          <main className="flex-1 p-4 sm:p-6">{children}</main>
+          <main className="flex-1 p-4 sm:p-6 overflow-y-auto">{children}</main>
         </SidebarInset>
       </SidebarProvider>
     </div>
