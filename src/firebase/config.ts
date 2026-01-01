369a3b9cc164fabae95bb4854b7f825f100e2439
Mr Lyrical <mrlyrical777@gmail.com>
2026-01-01 09:07:35 +0000

    make .env file and set it

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-7539630518-ae89f",
-  "appId": "1:784649478152:web:7e9c6250d566dfb28031bd",
-  "apiKey": "AIzaSyAvzXA88IG-5TlqvwZ3frFTOvQijPaBqzE",
-  "authDomain": "studio-7539630518-ae89f.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "784649478152"
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
