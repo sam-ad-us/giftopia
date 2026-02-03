aba9ba4f3de78db31e0cfdbd22ad184f7ef83c59
Mr Lyrical <mrlyrical777@gmail.com>
2026-02-03 12:09:13 +0000

    heigt of hero secton decrease by 50% of persent size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -272,7 +272,7 @@ export default function Home() {
 
   return (
     <div className="flex flex-col">
-      <section className="relative w-full h-[70vh] md:h-[80vh] flex items-center justify-center text-center text-white">
+      <section className="relative w-full h-[50vh] flex items-center justify-center text-center text-white">
         {isLoading ? (
           <Skeleton className="absolute inset-0" />
         ) : heroImage ? (
